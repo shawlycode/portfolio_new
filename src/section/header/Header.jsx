4c8bbf52a4b67979a943904dadc0fe5586e7e63a
Shawlycode <kutinaorlando@gmail.com>
2023-12-20 15:34:29 +0000

    header section added

--- src/section/header/Header.jsx
+++ src/section/header/Header.jsx
@@ -9,26 +9,23 @@ const Header = () => {
   return (
     <section id="header">
       <div className="container header__container">
-        <div className="header__left">
-          <h3>Hello I'm</h3>
-          <h2>Akwesi Adabo Boakye</h2>
-          <h4>Software Developer and Entrepreneur</h4>
+        <div className="left__container">
+          <p className="intro_p">Hello I'm</p>
+          <h2>Akwasi <span>Adabo</span> Boakye</h2>
           <p>A seasoned and versatile professional with a decade of experience in both software development and entrepreneurship, I have demonstrated a strong commitment to innovation, technical excellence, and business acumen. Throughout my career...</p>
           <div className="btn__container">
-            <button className="btn"> Hire Me</button>
-            <button className=" btn__white">Let's Collaborate</button>
+            <button className="btn">Hire Me</button>
+            <button className="btn btn__white">Let's Collaborate</button>
           </div>
         </div>
         <div className="header__right-profile">
           <img src={profile} alt="logo" />
         </div>
         <div className="header__socials">
-          {data.map(item => <ul key={item.key}>
-            <a href={item.link}>{item.icon}</a>
-          </ul>)}
+          {data.map(item => <a href={item.link} key={item.id}>{item.icon}</a>)}
         </div>
       </div>
-    </section>
+    </section >
   )
 }
 
--- src/section/header/data.jsx
+++ src/section/header/data.jsx
@@ -1,13 +1,13 @@
 
 import { FaInstagram } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
-
-
+import { FaTwitter } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa6";
 
 const data = [
   { id: 1, link: 'https://www.linkedin.com/', icon: <FaLinkedinIn /> },
   { id: 2, link: 'https://www.instagram.com/', icon: <FaInstagram /> },
-  { id: 3, link: 'https://www.linkedin.com/', icon: <FaLinkedinIn /> },
-  { id: 4, link: 'https://www.linkedin.com/', icon: <FaLinkedinIn /> },
+  { id: 3, link: 'https://www.linkedin.com/', icon: <FaTwitter /> },
+  { id: 4, link: 'https://www.linkedin.com/', icon: <FaGithub /> },
 ]
 export default data;
