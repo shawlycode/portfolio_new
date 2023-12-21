b3b8d201251e97158dfa11d8a2cb4d5a10b7f2ba
Shawlycode <kutinaorlando@gmail.com>
2023-12-21 00:07:53 +0000

    commit testimonials

--- src/section/portfolio/Portfolio.jsx
+++ src/section/portfolio/Portfolio.jsx
@@ -1,8 +1,11 @@
 import './portfolio.css'
-
+// import portfolio from '../../assets/pro.png'
 const Portfolio = () => {
   return (
-    <section id="portfolio"><h2>Portfolio</h2></section>
+    <section id="portfolio">
+      <h5>Portfolio</h5>
+      <h2>Latest <span>Projects</span></h2>
+    </section>
   )
 }
 
